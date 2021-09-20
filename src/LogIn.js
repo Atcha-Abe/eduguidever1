46178e9a9f2d1508d4136717a9561650a240d74f
MCBlankCams <camillemahilum@gmail.com>
2021-09-20 14:41:36 +0800

    Google Login and Facebook
    need if else statement for sign in and sign out process

--- src/LogIn.js
+++ src/LogIn.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import LoginHooks from './components/LoginHooks';
+import LogoutHooks from './components/LogoutHooks';
+import Facebook from './components/Facebook';
 import Welcome from './Welcome'
 import {
     BrowserRouter as Router,
     Route,
     Link
   } from 'react-router-dom'
+  import {
+    button,
+    buttonText,
+    icon
+  } from './components/Login.css'
+
+  
 export default class Contact extends Component {
     render() {
 
@@ -24,14 +34,19 @@ export default class Contact extends Component {
                     <b>Password:</b>
                         <input type="password" className="field" name="password" />
                     </label>
+                   
                 </form>
                 <p>Log In with</p>
                 <div align="center">
-                    <img src="./icons/Google.png" className="social-icons"></img>
-                    <img src="./icons/Facebook.png" className="social-icons"></img>
+                    <LoginHooks />
+                    <LogoutHooks />
+                    <Facebook />
+                    
                 </div>
                 <br></br>
                 <Link to="/welcome" className="reg-btn" value="Log In">Log In</Link>
+                    
+                
                 <p>Don't have an account yet? <br></br> Sign Up <b><Link to="/signup" style={{ textDecoration: 'none' }}>here</Link></b></p>
             </div>
         )
